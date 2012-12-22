d8a2f6afbddd3876384f621ed5600f3387e003e7
Nizar Khalife Iglesias <nizar@nobox.com>
2012-12-22 14:24:39 -0400

    Remove main.js.
    Signed-off-by: Nizar Khalife Iglesias <nizar@nobox.com>

--- scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.editable').click(function(){
-		var $el = $(this);
-		$('#tcjs-sidebar textarea').val($el.text());
-		$('#tcjs-sidebar input').val($el.data('blurb-id'));
-		console.log($el.text());
-	});
-})
