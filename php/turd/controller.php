aa3398c459fecf9d112f44a67f8b2658eeebce0c
Javier Velazquez <javieravelazquezhernandez@gmail.com>
2012-12-22 12:47:58 -0400

    Implemented turd.json to controller.

--- php/turd/controller.php
+++ php/turd/controller.php
@@ -6,10 +6,12 @@
 		private $lang_object;
 		private $file_path;
 
-		public function __construct($lang = 'es') { //Make path static always or insert the path
+		public function __construct($lang = 'turd') { //Make path static always or insert the path
 
 			$this->file_path = '../../lang/' . $lang . '.json';
 			$this->read_lang_doc();
+
+			//$this->debug($this->lang_object);
 		}
 
 		private function read_lang_doc(){
@@ -28,7 +30,8 @@
 
 		public function set_copy_string($id = 'glossary', $new_value = 'POOP'){
 			if($this->lang_object->{$id}){
-				$this->lang_object->{$id} = $new_value;
+				$this->lang_object->{$id}->text = $new_value;
+				$this->lang_object->{$id}->lastMod = time();
 				$this->write_new_json();
 			}else{
 				echo "No string of that name to edit";
@@ -65,5 +68,6 @@
 
 	$CController = new CController();
 	
-	//$CController->set_copy_string();
+	//$CController->set_copy_string('copyIdentifier', 'This is another test');
+	$CController->get_copy_string('copyIdentifier');
 
